078406651222d9f11e102fb16fa2f77bda2d9ad3
pmoranga <pmoranga@gmail.com>
2009-06-05 17:08:00 -0300

    adicionado organiza.pl

---
+++ organiza.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+#
+
+if (scalar(@ARGV)==0){
+    print "usage: $0 [FILES]\n";
+}
+
+@files=@ARGV;
+
+foreach $i (@files){
+	if ( $i =~ m/([ %+]|\..{4})/ ) {
+		my $o = $i;
+		$i =~ s/\n//g;
+		$o =~ s/\n//g;
+		$i =~ s/%(..)/pack("c",hex($1))/ge;
+		$i =~ s/\+/_/g;
+                $i =~ s/ /_/g;
+		$i = &trocaPonto($i);
+	        if ( rename($o, $i) ){
+			print "$o -> $i\n";
+		}
+		else {
+		        warn "Couldn't rename $o to $i: $!\n";
+		}
+	}
+}
+ 
+sub trocaPonto(){
+    $nome = shift(@_);
+    while ( $nome =~ m/\..{4}/){
+	$nome =~ s/\./_/;
+    }
+    return ($nome);
+}
